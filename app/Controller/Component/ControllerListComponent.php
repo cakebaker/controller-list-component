a0519a108cd478378ba1c968babc9df8f549b8dc
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2012-07-30 14:48:38 +0200

    Adding param $controllersToExclude to getList()

--- app/Controller/Component/ControllerListComponent.php
+++ app/Controller/Component/ControllerListComponent.php
@@ -1,24 +1,24 @@
 <?php
 
 class ControllerListComponent extends Component {
-    private $_excludedControllers = array('AppController', 'PagesController');
 
-    public function getList() {
-        $controllerClasses = App::objects('Controller');
+    public function getList(Array $controllersToExclude = array('PagesController')) {
+        $controllersToExclude[] = 'AppController';
+        $controllerClasses = array_filter(App::objects('Controller'), function ($controller) use ($controllersToExclude) {
+            return !in_array($controller, $controllersToExclude);
+        });
         $appControllerMethods = get_class_methods('AppController');
         $controllers = array();
 
         foreach($controllerClasses as $controller) {
-            if (!in_array($controller, $this->_excludedControllers)) {
-                App::uses($controller, 'Controller');
-                $methods = get_class_methods($controller);
-                foreach($methods as $k => $v) {
-                    if ($v{0} == '_') {
-                        unset($methods[$k]);
-                    }
+            App::uses($controller, 'Controller');
+            $methods = get_class_methods($controller);
+            foreach($methods as $k => $v) {
+                if ($v{0} == '_') {
+                    unset($methods[$k]);
                 }
-                $controllers[$controller] = array_diff($methods, $appControllerMethods);
             }
+            $controllers[$controller] = array_diff($methods, $appControllerMethods);
         }
 
         return $controllers;
