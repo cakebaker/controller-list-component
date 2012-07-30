ff9821a48f6484e828e02bd57919fd09d92d5328
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2012-07-30 15:25:28 +0200

    Refactoring: Extracting some private methods

--- app/Controller/Component/ControllerListComponent.php
+++ app/Controller/Component/ControllerListComponent.php
@@ -7,20 +7,33 @@ class ControllerListComponent extends Component {
         $controllerClasses = array_filter(App::objects('Controller'), function ($controller) use ($controllersToExclude) {
             return !in_array($controller, $controllersToExclude);
         });
-        $appControllerMethods = get_class_methods('AppController');
-        $controllers = array();
+        $result = array();
 
         foreach($controllerClasses as $controller) {
-            App::uses($controller, 'Controller');
-            $methods = get_class_methods($controller);
-            foreach($methods as $k => $v) {
-                if ($v{0} == '_') {
-                    unset($methods[$k]);
-                }
-            }
-            $controllers[$controller] = array_diff($methods, $appControllerMethods);
+            $result[$controller] = $this->getActions($controller);
         }
 
-        return $controllers;
+        return $result;
+    }
+
+    private function getActions($controller) {
+        App::uses($controller, 'Controller');
+        $methods = get_class_methods($controller);
+        $methods = $this->removeParentMethods($methods);
+        $methods = $this->removePrivateActions($methods);
+
+        return $methods;
+    }
+
+    private function removeParentMethods(Array $methods) {
+        $appControllerMethods = get_class_methods('AppController');
+
+        return array_diff($methods, $appControllerMethods);
+    }
+
+    private function removePrivateActions(Array $methods) {
+        return array_filter($methods, function ($method) {
+            return $method{0} != '_';
+        });
     }
 }
